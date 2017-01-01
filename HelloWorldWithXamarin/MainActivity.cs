013777e103dd2611e1b5f4b8ed0e0a02b3393455
PierreStephaneVoltaire <psvoltaire@yahoo.com>
2016-12-31 23:26:42 -0500

    Update MainActivity.cs

--- HelloWorldWithXamarin/MainActivity.cs
+++ HelloWorldWithXamarin/MainActivity.cs
@@ -1,4 +1,4 @@
-﻿using Android.App;
+using Android.App;
 using Android.Widget;
 using Android.OS;
 
@@ -7,19 +7,19 @@ namespace HelloWorldWithXamarin
     [Activity(Label = "HelloWorldWithXamarin", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        //variables to get the widgets on the activity
         Button btn;
         TextView hello;
         EditText name;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-// Set our view from the "main" layout resource
              SetContentView (Resource.Layout.Main);
-
-
+           // initialize widgets
             name =FindViewById< EditText > (Resource.Id.name);
             hello = FindViewById<TextView>(Resource.Id.Hello);
             btn = FindViewById<Button>(Resource.Id.button1);
+            //set the name when the button is clicked
             btn.Click += delegate {
                 if (!string.IsNullOrWhiteSpace(name.Text))
                     hello.Text = "Hello " + name.Text;
